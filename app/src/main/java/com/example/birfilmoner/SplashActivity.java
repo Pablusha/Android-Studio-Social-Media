1fcb99b15971a6a07e26e5c3caa21d625d14c921
Yasin Akdoğan <yasin_akdogan15@hotmail.com>
2020-07-20 02:58:05 +0300

    Splash activity tamamlandı, Login activity'nin ise arayüzü tamamlandı.

---
+++ app/src/main/java/com/example/birfilmoner/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.example.birfilmoner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private ImageView imgLogo;
+    private int GECIS_SURESI = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        imgLogo = findViewById(R.id.ac_splash_imgLogo);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,LoginActivity.class));
+                finish();
+            }
+        },GECIS_SURESI);
+
+    }
+
+}
