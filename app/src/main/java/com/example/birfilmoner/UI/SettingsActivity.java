6fbc3c11e5a7c642a3d18a4f197021b8ee137ac2
Yasin Akdoğan <yasin_akdogan15@hotmail.com>
2020-09-12 17:30:58 +0300

    Ayarlar arayüzüne toolbar eklendi.

--- app/src/main/java/com/example/birfilmoner/UI/SettingsActivity.java
+++ app/src/main/java/com/example/birfilmoner/UI/SettingsActivity.java
@@ -2,6 +2,7 @@ package com.example.birfilmoner.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.widget.CompoundButton;
@@ -11,12 +12,15 @@ import com.example.birfilmoner.R;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-
+        toolbar = findViewById(R.id.ac_settings_toolbar);
+        setSupportActionBar(toolbar);
 
     }
 }
